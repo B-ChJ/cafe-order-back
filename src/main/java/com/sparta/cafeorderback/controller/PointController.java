63cbb78462878bc119c360a538904725ec9f8f37
B-ChJ <bchaejoo@gmail.com>
2026-01-12 19:37:02 +0900

    refactor: 유효성 검증 적용
    - 의존성 추가
    - PointController 요청 유효성 검증 추가

--- src/main/java/com/sparta/cafeorderback/controller/PointController.java
+++ src/main/java/com/sparta/cafeorderback/controller/PointController.java
@@ -4,11 +4,13 @@ import com.sparta.cafeorderback.dto.ChargeRequest;
 import com.sparta.cafeorderback.dto.PointInfoResponse;
 import com.sparta.cafeorderback.security.CustomUserDetails;
 import com.sparta.cafeorderback.service.PointService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +23,7 @@ public class PointController {
 
   @PostMapping("/me/points/charge")
   public ResponseEntity<PointInfoResponse> charge(@AuthenticationPrincipal CustomUserDetails user,
-      ChargeRequest request) {
+      @RequestBody @Valid ChargeRequest request) {
     PointInfoResponse response = pointService.chargeMyPoint(user.getUsername(), request);
 
     return ResponseEntity.status(HttpStatus.CREATED).body(response);
--- src/main/java/com/sparta/cafeorderback/dto/ChargeRequest.java
+++ src/main/java/com/sparta/cafeorderback/dto/ChargeRequest.java
@@ -1,5 +1,7 @@
 package com.sparta.cafeorderback.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,6 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ChargeRequest {
 
+  @NotNull
+  @Positive
   private Long amount;
 
   public ChargeRequest(Long amount) {
