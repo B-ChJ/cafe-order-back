73462518a80569580cf777077368b198bee3b9ac
B-ChJ <bchaejoo@gmail.com>
2026-01-12 09:50:40 +0900

    Initial commit.

---
+++ src/main/java/com/sparta/cafeorderback/CafeOrderBackApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.cafeorderback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeOrderBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeOrderBackApplication.class, args);
+	}
+
+}
