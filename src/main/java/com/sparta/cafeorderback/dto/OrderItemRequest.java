19c9b271d0c715415048e8b5ef9a608ea61d9fc0
B-ChJ <bchaejoo@gmail.com>
2026-01-13 00:18:38 +0900

    feat: 주문 DTO 생성
    - OrderItem과 Order 간 연관관계 고려해 List<OrderItemResponse> 형태로 응답 구성.

---
+++ src/main/java/com/sparta/cafeorderback/dto/OrderItemRequest.java
@@ -0,0 +1,17 @@
+package com.sparta.cafeorderback.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class OrderItemRequest {
+
+    @NotNull
+    private Long menuId;
+
+    @Min(1)
+    private int quantity;
+}
