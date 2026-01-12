72dd401c3f3acf3a3e70ee8b0f0a34f42e756fdd
B-ChJ <bchaejoo@gmail.com>
2026-01-12 20:54:38 +0900

    feat: 사용자 로그인 API 추가 구현

--- src/main/java/com/sparta/cafeorderback/controller/AuthController.java
+++ src/main/java/com/sparta/cafeorderback/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.sparta.cafeorderback.controller;
 
+import com.sparta.cafeorderback.dto.LoginRequest;
+import com.sparta.cafeorderback.dto.LoginResponse;
 import com.sparta.cafeorderback.dto.RegisterRequest;
 import com.sparta.cafeorderback.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +23,14 @@ public class AuthController {
   @PostMapping("/register")
   public ResponseEntity<Void> register(@RequestBody @Valid RegisterRequest request) {
     authService.registerUser(request);
-    return ResponseEntity.ok().build();
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
+    LoginResponse response = authService.loginUser(request);
+
+    return ResponseEntity.status(HttpStatus.OK).body(response);
   }
 
 }
